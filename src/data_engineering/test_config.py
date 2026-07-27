a6a86fa7d0337cc00eedb21c31f2048b7f3cc5cd
captainlevi1911 <shelaraditya817@gmail.com>
2026-07-27 22:29:04 +0530

    Added test_config.py

---
+++ src/data_engineering/test_config.py
@@ -0,0 +1,8 @@
+from config import *
+
+print("Project Root :", PROJECT_ROOT)
+print("Data Folder  :", DATA_DIR)
+print("Raw Data     :", RAW_DATA_DIR)
+print("Monthly Data :", MONTHLY_DATA_DIR)
+print("Processed    :", PROCESSED_DATA_DIR)
+print("Logs         :", LOG_DIR)
